147306cbf302c7200dc701d73762eb2a3c8def2d
Matheus <matheussschingiry@gmail.com>
2025-05-22 20:50:04 -0300

    feat: client model

---
+++ src/main/java/com/DBank/DBank/model/client.java
@@ -0,0 +1,36 @@
+package com.DBank.DBank.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "tb_client")
+public class client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    @Column(unique = true)
+    private String register;
+
+    @NotBlank
+    @Column(unique = true)
+    private String email;
+
+    @NotBlank
+    private String password;
+
+    @NotBlank
+    private double amount;
+}
