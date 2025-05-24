52a2818eefc6db80960e91c533dfb6287b614942
Matheus <matheussschingiry@gmail.com>
2025-05-24 15:37:42 -0300

    feat: Enterprise Service

---
+++ src/main/java/com/DBank/DBank/service/EnterpiseService.java
@@ -0,0 +1,52 @@
+package com.DBank.DBank.service;
+
+import com.DBank.DBank.dto.EnterpriseDTO;
+import com.DBank.DBank.mapper.EnterpriseMapper;
+import com.DBank.DBank.model.EnterpriseModel;
+import com.DBank.DBank.repository.EnterpriseRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EnterpiseService {
+
+    private final EnterpriseRepository repository;
+    private final EnterpriseMapper mapper;
+
+    public EnterpiseService(EnterpriseRepository repository, EnterpriseMapper mapper) {
+        this.repository = repository;
+        this.mapper = mapper;
+    }
+
+    public EnterpriseDTO add(EnterpriseDTO dto){
+        EnterpriseModel model = mapper.toModel(dto);
+        repository.save(model);
+
+        return mapper.toDTO(model);
+    }
+
+    public EnterpriseDTO list(String email){
+        return repository.findByEmail(email)
+                .map(mapper::toDTO)
+                .orElseThrow(() -> new RuntimeException("Email not found"));
+    }
+
+    public EnterpriseDTO edit(String id, EnterpriseDTO dto){
+        EnterpriseModel model = repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Email not found"));
+
+        if(dto.getName() == null) dto.setName(model.getName());
+        if(dto.getEmail() == null) dto.setEmail(model.getEmail());
+        if(dto.getPassword() == null) dto.setPassword(model.getPassword());
+
+        dto.setId(id);
+        dto.setRegister(model.getRegister());
+        dto.setAmount(model.getAmount());
+
+        EnterpriseModel editUser = repository.save(mapper.toModel(dto));
+        return mapper.toDTO(editUser);
+    }
+
+    public void delete(String id){
+        repository.deleteById(id);
+    }
+}
