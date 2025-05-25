c7acd00d1e7da71590a95536dbddd53457188513
Matheus <matheussschingiry@gmail.com>
2025-05-25 20:16:22 -0300

    feat: Transaction Controller

---
+++ src/main/java/com/DBank/DBank/controller/TransactionController.java
@@ -0,0 +1,23 @@
+package com.DBank.DBank.controller;
+
+import com.DBank.DBank.dto.TransactionDTO;
+import com.DBank.DBank.service.TransactionService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/transaction")
+public class TransactionController {
+
+    private final TransactionService service;
+
+    public TransactionController(TransactionService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/add")
+    public TransactionDTO add(TransactionDTO dto){
+        return service.Operation(dto);
+    }
+}
